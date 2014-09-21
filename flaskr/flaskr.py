404eecb2ba3bc21372e4189d7b71c3465fb3fefb
MalkaR <mmrabinowitz@gmail.com>
2014-09-21 17:24:21 -0400

    yaml config file converted to validation schema. put request tests pass, still need config --> validation tests

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -2,7 +2,8 @@ import sqlite3
 import us
 import csv
 import os
-import json
+import yaml
+# import json
 from flask import Flask, g, request, make_response
 from flask.ext.restful import reqparse, abort, Api, Resource
 from flask.ext.sqlalchemy import SQLAlchemy
@@ -14,6 +15,90 @@ app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 api = Api(app)
 db = SQLAlchemy(app)
 US_STATES = [state.abbr for state in us.states.STATES]
+
+order_fields_file = open('/Users/malka/Documents/job/Lot18Code/flaskr/config/field_validators.yaml')
+# use safe_load instead load
+order_fields = yaml.safe_load(order_fields_file)
+order_fields_file.close()
+
+# print order_fields
+
+def forbidden_values(values_list=[], column_name=None):
+    """
+    The field's value can not be one of the values_list items.
+    """
+    def f(v):
+        if v in values_list:
+            msg = 'Value: {}; Error: This is not an allowed value'.format(v)
+            if column_name:
+                msg = 'Field: {}; '.format(column_name) + msg
+            raise Invalid(msg)
+        return v
+
+    return f
+
+def coerce_to_type(typ=int, column_name=None):
+    if type(typ) == str:
+        typ = eval(typ)
+
+    def f(v):
+        try:
+            return typ(v)
+        except ValueError as e:
+            msg = 'Value: {}; Error: can not be converted to the {} type'.format(str(v), typ)
+            if column_name:
+                msg = 'Field: {}; '.format(column_name) + msg
+            raise Invalid(msg)
+    return f
+
+class Struct:
+    def __init__(self, **entries):
+        self.schema = {}
+        self.__dict__.update(entries)
+        if not hasattr(self, 'required_fields'):
+            self.__dict__.update({'required_fields':{}})
+        else:
+            self.complete_required_field_dictionaries(self.required_fields)
+
+        if not hasattr(self, 'optional_fields'):
+            self.__dict__.update({'optional_fields':{}})
+        else:
+            self.complete_optional_field_dictionaries(self.optional_fields)
+        
+
+    def complete_required_field_dictionaries(self, required_field_dict):
+        for field_name, field_validators in required_field_dict.items():
+            self.schema[Required(field_name)] = self.get_field_validators(field_validators)
+
+    def complete_optional_field_dictionaries(self, optional_field_dict):
+        for field_name, field_validators in optional_field_dict.items():
+            self.schema[field_name] = self.get_field_validators(field_validators)
+    
+    def get_field_validators(self, field_validators):
+            args = []
+            if 'type' in field_validators:
+                args.append(eval(field_validators['type']))
+            if 'functions' in field_validators:
+                for function_name, function_args in field_validators['functions'].items():
+                    # if type(function_name) == str:
+                    #     args.append(eval(function_name)())
+                    # else:
+                    args.append(eval(function_name)(*tuple(function_args.get('args', [])), **function_args.get('kwargs', {})))
+            if len(args) > 1:
+                return All(*tuple(args))
+            return args[0]
+    
+    def get_schema(self):
+        return self.schema
+
+    def __repr__(self):
+        return str(self.__dict__)
+
+
+def create_schema_from_config(config_dict):
+    schema_dict = Struct(**config_dict['order_fields'])
+    schema = Schema(schema_dict.schema)
+    return schema
 
 
 # Database Model
@@ -38,39 +123,20 @@ class Order(db.Model):
         return '<Order %d>' % self.id
 
 # Order validation 
-def coerce(typ=int, column_name=None):
-    
-    def f(v):
-        try:
-            return typ(v)
-        except ValueError as e:
-            msg = 'Value: {}; Error: can not be converted to the {} type'.format(str(v), typ)
-            if column_name:
-                msg = 'Field: {}; '.format(column_name) + msg
-            raise Invalid(msg)
-    return f
 
-def allowed_states():
-    """
-    No wine can ship to New Jersey, Connecticut, Pennsylvania, Massachusetts,
-    Illinois, Idaho or Oregon
-    """
-    def f(v):
-        if v in ['NJ', 'CT', 'PA', 'MA', 'OR', 'ID', 'IL']:
-            msg = 'Field: state; Value: {}; Error: Wine can not ship to this state'.format(v)
-            raise Invalid(msg)
-        return v
 
-    return f
 
-schema = Schema({
-    Required('id'): All(coerce(int, column_name='order_id'),),
-    Required('name'): All(str, Length(min=1)),
-    'email': str,
-    'birthday' : date,
-    'state': All(str, allowed_states()),
-    'zipcode': str
-})
+
+# schema = Schema({
+#     Required('id'): All(coerce_to_type(int, column_name='order_id'),),
+#     Required('name'): All(str, Length(min=1)),
+#     'email': str,
+#     'birthday' : date,
+#     'state': All(str, forbidden_values(values_list=['NJ', 'CT', 'PA', 'MA', 'OR', 'ID', 'IL'], column_name='state')),
+#     'zipcode': str
+# })
+
+schema = create_schema_from_config(order_fields)
 
 class Orders(Resource):
     def get(self):
